b66616f455caabf3ac82d82eb62549b59106d188
jibundeyare <contact@jibundeyare.com>
2023-06-13 15:17:35 +0200

    Cours sur les opérateurs

--- operators.js
+++ operators.js
@@ -12,3 +12,23 @@ console.log(text1 == number1);
 // renvoie false
 console.log(number1 === text1);
 console.log(text1 === number1);
+
+// opérateur d'incrémentation
+console.log(number1)
+
+// number1 = number1 + 1
+// number1 += 1
+number1++;
+console.log(number1);
+
+// attention : l'incrémentation se fait après l'affichage
+console.log(number1++);
+console.log(number1);
+
+// solution : l'incrémentation se fait avant l'affichage
+console.log(++number1);
+
+// number1 = number1 - 1
+// number1 -= 1
+number1--;
+console.log(number1);
