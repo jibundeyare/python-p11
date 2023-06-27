3f71fd1510df87eecaf18781568036f29223fba1
jibundeyare <contact@jibundeyare.com>
2023-06-27 09:41:46 +0200

    Cours sur les fonctions

--- functions.py
+++ functions.py
@@ -48,3 +48,54 @@ print(result)
 
 # le nom de la fonction + ses paramètres + sont type de retour = signature de la fonction
 # def mult(a: int, b: int) -> int:
+
+# copie d'une fonction comme si c'était une variable
+mult_copy = mult
+mult_copy(2, 5)
+
+# stockage de fonctions dans une liste
+operations = []
+operations.append(addition)
+operations.append(mult)
+
+a = 2
+b = 5
+resultat = None
+
+for operation in operations:
+    resultat = operation(a, b)
+    print(resultat)
+
+# fonction de degré supérieur
+# c'est une fonction qui accepte une fonction en paramètre
+# ou qui renvoie une fonction
+def operateur_binaire(a, b, fonction):
+    return fonction(a, b)
+
+# appel de la fonction de degré supérieur
+resultat = operateur_binaire(2, 5, mult)
+
+my_list = ['foo', "ipsum"]
+text = "toto"
+
+print(len(my_list))
+print(len(text))
+
+def my_len(value):
+    return 42
+
+# sauvegarde de la fonction len() originale
+len_backup = len
+# surcharge de la fonction len() originale
+# c-à-d remplacement par une autre fonction
+len = my_len
+
+print(len(my_list))
+print(len(text))
+
+# restauration de la fonction len() originale
+len = len_backup
+
+# pass permet d'écrire du code python syntaxiquement valide même quand on n'a pas encore le corps du la condition if ou de la boucle for
+if True:
+    pass
